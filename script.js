4dd28bb0319005d17c8cc0c2f6e5a89b4d1bff0f
HarithaPraveen1 <142241348+HarithaPraveen1@users.noreply.github.com>
2023-09-06 23:09:15 -0400

    added

---
+++ script.js
@@ -0,0 +1,10 @@
+const sideMenu = document.getElementById("side-menu")
+
+function openmenu(){
+    sideMenu.style.right ="0"
+    console.log("hi");
+}
+function closemenu() {
+    sideMenu.style.right ="-200px"
+    console.log("hii");
+}
